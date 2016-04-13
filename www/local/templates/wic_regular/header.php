c76097728ecf3462b8a919820c84241ac0280d03
Евгений <wowneyron@gmail.com>
2016-04-13 20:24:10 +0700

    Банки, виды инвистиций

--- www/local/templates/wic_regular/header.php
+++ www/local/templates/wic_regular/header.php
@@ -4,4 +4,22 @@ IncludeTemplateLangFile(dirname(__FILE__));
 
 require($_SERVER['DOCUMENT_ROOT'] . WIC_TEMPLATE_PATH . '/header.php');
 ?>
-        
+<div class="content-line">
+</div>
+<div class="content-line">
+    <div class="row">
+        <div class="columns small-12">
+            <nav class="breadcrumbs">
+                <a href="/">Куда вложить</a>
+                <a class="current" href="/banks">Банки</a>
+
+            </nav>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="columns small-12 medium-7 large-8">
+
+
+
+
