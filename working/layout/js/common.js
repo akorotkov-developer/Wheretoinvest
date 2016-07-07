b49a57a885aee2a344a23b5bf22ff1763b586517
LexOchirov <vergil.009@mail.ru>
2016-07-07 10:23:36 +0300

    Выпадающее меню юи начало таблички - для мобильных.

--- working/layout/js/common.js
+++ working/layout/js/common.js
@@ -1,18 +1,19 @@
-(function($){
-  $(function() {
-  "use strict";
+(function ($) {
+    $(function () {
+        "use strict";
 
 
-    $(document).foundation();
-    
-    // your code goes here
+        $(document).foundation();
 
-    (function(){
-      $('.js-show-menu').on('click',function(){
-          $('.js-menu').toggle(500);
-      });
-    })();
+        // your code goes here
+
+        (function () {
+            $('.js-show-menu').on('click', function () {
+                $('.js-menu').toggle(500);
+                $('.b-header__place, .b-header__firstline').toggleClass('js-toggle');
+            });
+        })();
 
 
-  });
+    });
 })(jQuery);
