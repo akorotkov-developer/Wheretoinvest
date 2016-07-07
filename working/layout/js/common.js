cc9f77a22c66b853f55f4e5a2225b4206da5f6dc
LexOchirov <vergil.009@mail.ru>
2016-07-07 16:39:31 +0300

    Пожелания клиента. Выпадающие меню и мелки правки.

--- working/layout/js/common.js
+++ working/layout/js/common.js
@@ -12,7 +12,37 @@
                 $('.js-menu').toggle(500);
                 $('.b-header__place, .b-header__showMenu,.b-header__firstline').toggleClass('js-toggle');
             });
+            //begin of sum menu show/hide
+            function sortText () {
+                var sum  = $('.b-sort__main .b-sort__inp').val() || 0;
+                var currency = $('.b-sort__main select').eq(0).val();
+                var time =  $('.b-sort__main select').eq(1).val();
+
+                $('.b-sort__all').text(sum + " " + currency + " на " + time);
+            }
+            $('.b-sort__arr').click(function () {
+                $('.b-sort__main').slideToggle();
+                $('.b-sort__all').toggleClass('js-toggle');
+                sortText ();
+            });
+
+            $(".b-sort__all").click(function () {
+                $('.b-sort__main').slideDown();
+                $(this).removeClass('js-toggle')
+                sortText ();
+            });
+            //end of sum menu show/hide
         })();
+
+        (function () {
+            function number_format(str) {
+                return str.replace(/(\s)+/g, '').replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ');
+            }
+
+            $('.b-sort__inp').keyup(function (event) {
+                $(this).val(number_format($(this).val()));
+            });
+        })()
 
 
     });
