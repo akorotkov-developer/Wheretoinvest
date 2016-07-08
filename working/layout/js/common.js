73c3736338b1ffd872a184dd0229a75e77cce2bf
LexOchirov <vergil.009@mail.ru>
2016-07-08 07:24:05 +0300

    Исправления по желанию клиента.

--- working/layout/js/common.js
+++ working/layout/js/common.js
@@ -13,23 +13,26 @@
                 $('.b-header__place, .b-header__showMenu,.b-header__firstline').toggleClass('js-toggle');
             });
             //begin of sum menu show/hide
-            function sortText () {
-                var sum  = $('.b-sort__main .b-sort__inp').val() || 0;
+            function sortText() {
+                var sum = $('.b-sort__main .b-sort__inp').val() || 0;
                 var currency = $('.b-sort__main select').eq(0).val();
-                var time =  $('.b-sort__main select').eq(1).val();
+                var time = $('.b-sort__main select').eq(1).val();
 
                 $('.b-sort__all').text(sum + " " + currency + " на " + time);
             }
+
             $('.b-sort__arr').click(function () {
                 $('.b-sort__main').slideToggle();
-                $('.b-sort__all').toggleClass('js-toggle');
-                sortText ();
+                $('.b-sort__arr,.b-sort__all').toggleClass('js-toggle');
+
+                sortText();
             });
 
             $(".b-sort__all").click(function () {
                 $('.b-sort__main').slideDown();
-                $(this).removeClass('js-toggle')
-                sortText ();
+                $(this).removeClass('js-toggle');
+                $('.b-sort__arr').removeClass('js-toggle');
+                sortText();
             });
             //end of sum menu show/hide
         })();
