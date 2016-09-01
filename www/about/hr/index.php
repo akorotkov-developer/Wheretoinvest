6135d5d96520752a912723a1f241b4fc2c1ecbc3
Leonid Chesnokov <leon.chesnokov@gmail.com>
2016-09-01 10:56:01 +0300

    http://pm.cetera.ru/browse/WIC-351

--- www/about/hr/index.php
+++ www/about/hr/index.php
@@ -2,6 +2,5 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Вакансии");
 ?>
-<link href="style.css" rel="stylesheet">
 <p class="novac">В настоящее время нет свободных вакансий</p>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
