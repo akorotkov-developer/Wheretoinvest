0f156036dca7614db65128f7fa7cb0b0ae7edc6d
Arthur Garin <a.garin@cetera.ru>
2016-09-07 13:12:14 +0300

    http://pm.cetera.ru/browse/WIC-342 9. Добавить прямоугольник "ВЕКСЕЛЯ и СЕРТИФИКАТЫ" между Банковскими вкладами и Займами

--- www/.top.menu.php
+++ www/.top.menu.php
@@ -1,32 +1,46 @@
 <?
 $aMenuLinks = Array(
-	Array(
-		"Все способы", 
-		"/",
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Банковские вклады", 
-		"/?method=3",
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Займы", 
-		"/?method=6",
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Избранные", 
-		"/?favorite=Y",
-		Array(), 
-		Array("class"=>"b-header__menu-link_star"), 
-		"" 
-	)
+    Array(
+        "Все способы",
+        "/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Банковские вклады",
+        "/?method=3",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Векселя",
+        "/?method=31",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Сертификаты",
+        "/?method=32",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Займы",
+        "/?method=6",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Избранные",
+        "/?favorite=Y",
+        Array(),
+        Array("class" => "b-header__menu-link_star"),
+        ""
+    )
 );
 ?>
