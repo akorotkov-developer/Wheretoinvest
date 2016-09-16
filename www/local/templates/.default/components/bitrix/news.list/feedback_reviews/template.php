8a892767d38fd1cbfa0361c865d83555814e1278
a.kremenitsky <mymailak@mail.ru>
2016-09-16 15:29:16 +0300

    http://pm.cetera.ru/browse/WIC-406

---
+++ www/local/templates/.default/components/bitrix/news.list/feedback_reviews/template.php
@@ -0,0 +1,46 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="reviews">
+<?foreach($arResult["ITEMS"] as $arItem):?>
+	<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+	<div class="reviews-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+		<div class="reviews-item_author">
+			<span class="reviews-item_author-name"><?echo $arItem["DISPLAY_PROPERTIES"]["REVIEW_AUTHOR_NAME"]["VALUE"]?></span>
+		</div>
+		<div class="reviews-item_rating">
+			<?
+			$stars = 0;
+			$rating = intval($arItem["DISPLAY_PROPERTIES"]["REVIEW_RATING"]["VALUE"]);
+			while ($stars < 5) {
+				if ($rating > $stars) {
+					echo('<span class="reviews-item_rating-star reviews-item_rating-star-active"></span>');
+				} else {
+					echo('<span class="reviews-item_rating-star"></span>');
+				}
+				$stars++;
+			}?>
+		</div>
+		<div class="reviews-item_name"><?echo $arItem["NAME"]?></div>
+		<div class="reviews-item_text"><?echo $arItem["PREVIEW_TEXT"];?></div>
+		<div class="reviews-item_date"><?echo date("d.m.y", strtotime($arItem['DATE_CREATE']));?></div>
+	</div>
+<?endforeach;?>
+<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+	<br /><?=$arResult["NAV_STRING"]?>
+<?endif;?>
+</div>
