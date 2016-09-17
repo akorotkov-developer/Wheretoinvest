88df9f6245fefe3be7dacfeeddb0e4ab9c10b5ac
a.kremenitsky <mymailak@mail.ru>
2016-09-17 14:39:41 +0300

    http://pm.cetera.ru/browse/WIC-354

--- www/about/feedback/index.php
+++ www/about/feedback/index.php
@@ -3,6 +3,59 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Пожелания и отзывы");
 ?>
 
+<?$APPLICATION->IncludeComponent(
+	"bitrix:iblock.element.add.form", 
+	"add_review", 
+	array(
+		"SEF_MODE" => "Y",
+		"IBLOCK_TYPE" => "reviews",
+		"IBLOCK_ID" => "9",
+		"PROPERTY_CODES" => array(
+			0 => "1",
+			1 => "2",
+			2 => "NAME",
+			3 => "PREVIEW_TEXT",
+		),
+		"PROPERTY_CODES_REQUIRED" => array(
+			0 => "1",
+			1 => "2",
+			2 => "NAME",
+			3 => "PREVIEW_TEXT",
+		),
+		"GROUPS" => array(
+			0 => "2",
+		),
+		"STATUS_NEW" => "NEW",
+		"STATUS" => "INACTIVE",
+		"LIST_URL" => "",
+		"ELEMENT_ASSOC" => "CREATED_BY",
+		"ELEMENT_ASSOC_PROPERTY" => "1",
+		"MAX_USER_ENTRIES" => "100000",
+		"MAX_LEVELS" => "100000",
+		"LEVEL_LAST" => "Y",
+		"USE_CAPTCHA" => "Y",
+		"USER_MESSAGE_EDIT" => "",
+		"USER_MESSAGE_ADD" => "",
+		"DEFAULT_INPUT_SIZE" => "30",
+		"RESIZE_IMAGES" => "Y",
+		"MAX_FILE_SIZE" => "0",
+		"PREVIEW_TEXT_USE_HTML_EDITOR" => "N",
+		"DETAIL_TEXT_USE_HTML_EDITOR" => "N",
+		"CUSTOM_TITLE_NAME" => "",
+		"CUSTOM_TITLE_TAGS" => "",
+		"CUSTOM_TITLE_DATE_ACTIVE_FROM" => "",
+		"CUSTOM_TITLE_DATE_ACTIVE_TO" => "",
+		"CUSTOM_TITLE_IBLOCK_SECTION" => "",
+		"CUSTOM_TITLE_PREVIEW_TEXT" => "",
+		"CUSTOM_TITLE_PREVIEW_PICTURE" => "",
+		"CUSTOM_TITLE_DETAIL_TEXT" => "",
+		"CUSTOM_TITLE_DETAIL_PICTURE" => "",
+		"SEF_FOLDER" => "/",
+		"COMPONENT_TEMPLATE" => "add_review"
+	),
+	false
+);?>
+
 <?$APPLICATION->IncludeComponent("bitrix:news.list","feedback_reviews",Array(
         "DISPLAY_DATE" => "Y",
         "DISPLAY_NAME" => "Y",
---
+++ www/local/templates/.default/components/bitrix/iblock.element.add.form/add_review/template.php
@@ -0,0 +1,113 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(false);
+
+if (!empty($arResult["ERRORS"])):?>
+	<?ShowError(implode("<br />", $arResult["ERRORS"]))?>
+<?endif;
+if (strlen($arResult["MESSAGE"]) > 0):?>
+	<?ShowNote($arResult["MESSAGE"])?>
+<?endif?>
+
+<?
+$fields = array("1","2","NAME","PREVIEW_TEXT");
+?>
+<form name="iblock_add" action="<?=POST_FORM_ACTION_URI?>" method="post" enctype="multipart/form-data">
+	<?=bitrix_sessid_post()?>
+	<div class="reviews-form">
+		<?if (is_array($arResult["PROPERTY_LIST"]) && !empty($arResult["PROPERTY_LIST"])):?>
+		<div class="reviews-form_content">
+			<?foreach ($fields as $field):?>
+				<?if (isset($arResult["PROPERTY_LIST_FULL"][$field])) :?>
+					<div class="reviews-form_content-item">
+						<div class="reviews-form_content-item-name">
+							<?if (intval($field) > 0):?>
+								<?=$arResult["PROPERTY_LIST_FULL"][$field]["NAME"]?>
+							<?else:?>
+								<?=!empty($arParams["CUSTOM_TITLE_".$field]) ? $arParams["CUSTOM_TITLE_".$field] : GetMessage("IBLOCK_FIELD_".$field)?>
+							<?endif?>
+
+							<?if(in_array($field, $arResult["PROPERTY_REQUIRED"])):?>
+								<span class="starrequired">*</span>
+							<?endif?>
+						</div>
+						<div class="reviews-form_content-item-value">
+							<?
+							switch ($field):
+								case "1":
+								case "NAME":
+									?>
+									<input type="text" name="PROPERTY[<?=$field?>][0]" size="25" />
+									<?
+								break;
+
+								case "PREVIEW_TEXT":
+									?>
+									<textarea cols="<?=$arResult["PROPERTY_LIST_FULL"][$field]["COL_COUNT"]?>" rows="<?=$arResult["PROPERTY_LIST_FULL"][$field]["ROW_COUNT"]?>" name="PROPERTY[<?=$field?>][0]"></textarea>
+									<?
+								break;
+
+								case "2":
+									?>
+									<div id="reviews-rating">
+										<?foreach (array_reverse($arResult["PROPERTY_LIST_FULL"][$field]["ENUM"], true) as $key => $arEnum) {?>
+											<input type="radio" class="rating-item" id="rating-<?=$key?>" name="PROPERTY[<?=$field?>]" value="<?=$key?>">
+											<label id="star-<?=$key?>" for="rating-<?=$key?>"></label>
+										<?}?>
+									</div>
+									<?
+								break;
+							endswitch;?>
+						</div>
+					</div>
+				<?endif;?>
+			<?endforeach;?>
+
+			<?if($arParams["USE_CAPTCHA"] == "Y" && $arParams["ID"] <= 0):?>
+				<div class="reviews-form_content-item">
+					<div class="reviews-form_content-item-name">
+						<?=GetMessage("IBLOCK_FORM_CAPTCHA_TITLE")?>
+					</div>
+					<div class="reviews-form_content-item-value">
+						<input type="hidden" name="captcha_sid" value="<?=$arResult["CAPTCHA_CODE"]?>" />
+						<img src="/bitrix/tools/captcha.php?captcha_sid=<?=$arResult["CAPTCHA_CODE"]?>" width="180" height="40" alt="CAPTCHA" />
+					</div>
+				</div>
+				<div class="reviews-form_content-item">
+					<div class="reviews-form_content-item-name">
+						<?=GetMessage("IBLOCK_FORM_CAPTCHA_PROMPT")?><span class="starrequired">*</span>:
+					</div>
+					<div class="reviews-form_content-item-value">
+						<input type="text" name="captcha_word" maxlength="50" value="">
+					</div>
+				</div>
+			<?endif?>
+		</div>
+		<?endif?>
+		<div class="reviews-form_footer">
+			<div class="reviews-form_footer-button">
+				<input type="submit" name="iblock_submit" value="<?=GetMessage("IBLOCK_FORM_SUBMIT")?>" />
+				<?if (strlen($arParams["LIST_URL"]) > 0):?>
+					<input type="submit" name="iblock_apply" value="<?=GetMessage("IBLOCK_FORM_APPLY")?>" />
+					<input
+						type="button"
+						name="iblock_cancel"
+						value="<? echo GetMessage('IBLOCK_FORM_CANCEL'); ?>"
+						onclick="location.href='<? echo CUtil::JSEscape($arParams["LIST_URL"])?>';"
+					>
+				<?endif?>
+			</div>
+		</div>
+	</div>
+</form>
