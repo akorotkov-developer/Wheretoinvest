392d26355229f0096bacbb1e273432cb8fcb297f
Arthur Garin <a.garin@cetera.ru>
2016-10-03 09:50:49 +0300

    http://pm.cetera.ru/browse/WIC-439 Вид главной страницы

--- www/index.php
+++ www/index.php
@@ -2,14 +2,6 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Куда вложить деньги");
 ?>
-<? $APPLICATION->IncludeComponent("bitrix:main.include", "", Array(
-        "AREA_FILE_SHOW" => "file",
-        "AREA_FILE_SUFFIX" => "",
-        "PATH" => "/include/main_info.php",
-        "AREA_FILE_RECURSIVE" => "Y",
-        "EDIT_TEMPLATE" => "standard.php"
-    )
-); ?>
     <br class="show-for-medium">
 <? $APPLICATION->IncludeComponent("bitrix:main.include", "", Array(
         "AREA_FILE_SHOW" => "file",
