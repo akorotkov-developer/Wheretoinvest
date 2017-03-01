b282448b7e39d8bba9f3777297cc127f3fae7296
Leonid Chesnokov <leon.chesnokov@gmail.com>
2017-03-01 09:06:25 +0300

    http://pm.cetera.ru/browse/WIC-544

--- www/local/components/cetera/super.component/templates/offer.list/result_modifier.php
+++ www/local/components/cetera/super.component/templates/offer.list/result_modifier.php
@@ -131,7 +131,7 @@ if (empty($_REQUEST["summ"]))
     $_REQUEST["summ"] = "1 500 000";
 
 if (empty($_REQUEST["time"]))
-    $_REQUEST["time"] = "365";
+    $_REQUEST["time"] = "369";
 
 if (count($offers)) {
     $filter["UF_OFFER"] = $offers;
