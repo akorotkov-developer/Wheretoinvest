3c587b7c344035dc3ced76ae68b023fe6b357fa0
Arthur Garin <a.garin@cetera.ru>
2017-07-24 13:17:02 +0300

    https://pm.cetera.ru/browse/WIC-611 Защита персональных данных

--- www/local/templates/.default/components/bitrix/system.auth.registration/.default/template.php
+++ www/local/templates/.default/components/bitrix/system.auth.registration/.default/template.php
@@ -9,6 +9,7 @@
 /**
  * Bitrix vars
  * @global CMain $APPLICATION
+ *
  * @param array $arParams
  * @param array $arResult
  * @param CBitrixComponentTemplate $this
@@ -56,7 +57,7 @@ $emailConfirm = COption::GetOptionString("main", "new_user_registration_email_co
                 $arResult["FORM_FIELDS_PERSON"] = Array(
                     "PERSONAL_GENDER" => Array(
                         "TYPE" => "RADIO",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["PERSONAL_GENDER"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["PERSONAL_GENDER"] : "",
                         "BLOCK_TITLE" => "Пол",
                         "LIST" => Array(
                             "M" => "Мужской",
@@ -67,7 +68,7 @@ $emailConfirm = COption::GetOptionString("main", "new_user_registration_email_co
                     "UF_BIRTHDAY" => Array(
                         "BLOCK_TITLE" => "Год рождения",
                         "TYPE" => "TEXT",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["UF_BIRTHDAY"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["UF_BIRTHDAY"] : "",
                         "REQUIRED" => "Y",
                         "PLACEHOLDER" => "гггг",
                         "PARAMS" => Array(
@@ -78,13 +79,13 @@ $emailConfirm = COption::GetOptionString("main", "new_user_registration_email_co
                     "NAME" => Array(
                         "BLOCK_TITLE" => "Ваше имя для сайта",
                         "TYPE" => "TEXT",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["NAME"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["NAME"] : "",
                         "REQUIRED" => "Y",
                     ),
                     "USER_EMAIL" => Array(
                         "BLOCK_TITLE" => "Электронная почта",
                         "TYPE" => "EMAIL",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $arResult["USER_EMAIL"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $arResult["USER_EMAIL"] : "",
                         "REQUIRED" => "Y",
                         "DESCRIPTION" => "Будет логином на сайт. Вы должны иметь к ней доступ, чтобы подтвердить регистрацию",
                         "PARAMS" => Array(
@@ -94,10 +95,20 @@ $emailConfirm = COption::GetOptionString("main", "new_user_registration_email_co
                     "USER_PASSWORD" => Array(
                         "BLOCK_TITLE" => "Придумайте пароль",
                         "TYPE" => "PASSWORD",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $arResult["USER_PASSWORD"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $arResult["USER_PASSWORD"] : "",
                         "REQUIRED" => "Y",
                         "PARAMS" => Array(
                             "autocomplete" => "off"
+                        )
+                    ),
+                    "CONFIRM_C" => Array(
+                        "NO_TITLE" => "Y",
+                        "TYPE" => "CHECKBOX",
+                        "VALUE" => "",
+                        "REQUIRED" => "Y",
+                        "SINGLE" => "Y",
+                        "LIST" => Array(
+                            "Y" => 'Я согласен с условиями <a href="/upload/uf/63d/%D0%9F%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5%20%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5%202017.07.01.pdf" target="_blank">Пользовательского соглашения</a> и <a href="/upload/uf/307/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%202017.07.01.pdf" target="_blank">Политики конфиденциальности</a>'
                         )
                     ),
                 );
@@ -130,26 +141,26 @@ $emailConfirm = COption::GetOptionString("main", "new_user_registration_email_co
                     "WORK_COMPANY" => Array(
                         "BLOCK_TITLE" => "Сокращенное наименование организации (согласно уставу)",
                         "TYPE" => "TEXT",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["WORK_COMPANY"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["WORK_COMPANY"] : "",
                         "REQUIRED" => "Y",
                     ),
                     "NAME" => Array(
                         "BLOCK_TITLE" => "Контактное лицо",
                         "TYPE" => "TEXT",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["NAME"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["NAME"] : "",
                         "REQUIRED" => "Y",
                     ),
                     "PERSONAL_PHONE" => Array(
                         "BLOCK_TITLE" => "Контактный телефон",
                         "TYPE" => "TEXT",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["PERSONAL_PHONE"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $_REQUEST["PERSONAL_PHONE"] : "",
                         "REQUIRED" => "Y",
                         "INPUT_CLASS" => "js-phone"
                     ),
                     "USER_EMAIL" => Array(
                         "BLOCK_TITLE" => "Электронная почта",
                         "TYPE" => "EMAIL",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $arResult["USER_EMAIL"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $arResult["USER_EMAIL"] : "",
                         "REQUIRED" => "Y",
                         "DESCRIPTION" => "Будет логином на сайт. Вы должны иметь к ней доступ, чтобы подтвердить регистрацию",
                         "PARAMS" => Array(
@@ -159,10 +170,20 @@ $emailConfirm = COption::GetOptionString("main", "new_user_registration_email_co
                     "USER_PASSWORD" => Array(
                         "BLOCK_TITLE" => "Придумайте пароль",
                         "TYPE" => "PASSWORD",
-                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $arResult["USER_PASSWORD"]: "",
+                        "VALUE" => $arParams["~AUTH_RESULT"]["TYPE"] !== "OK" ? $arResult["USER_PASSWORD"] : "",
                         "REQUIRED" => "Y",
                         "PARAMS" => Array(
                             "autocomplete" => "off"
+                        )
+                    ),
+                    "CONFIRM_P" => Array(
+                        "NO_TITLE" => "Y",
+                        "TYPE" => "CHECKBOX",
+                        "VALUE" => "",
+                        "REQUIRED" => "Y",
+                        "SINGLE" => "Y",
+                        "LIST" => Array(
+                            "Y" => 'Я согласен с условиями <a href="/upload/uf/63d/%D0%9F%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5%20%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5%202017.07.01.pdf" target="_blank">Пользовательского соглашения</a> и <a href="/upload/uf/307/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%202017.07.01.pdf" target="_blank">Политики конфиденциальности</a>'
                         )
                     ),
                 );
@@ -193,3 +214,19 @@ $emailConfirm = COption::GetOptionString("main", "new_user_registration_email_co
         </div>
     </div>
 </div>
+<script type="text/javascript">
+    $(function () {
+        $("[name='bform']").on("submit", function () {
+            var id = "";
+            if ($(this).find("#FIELD_CONFIRM_C").length)
+                id = "#FIELD_CONFIRM_C";
+            else
+                id = "#FIELD_CONFIRM_P";
+
+            if (!$(id).is(":checked")) {
+                alert("Для продолжения Вам необходимо согласиться с условиями Пользовательского соглашения и Политики конфиденциальности.");
+                return false;
+            }
+        });
+    });
+</script>
