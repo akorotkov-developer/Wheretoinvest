36edbb373a8181e392f8c0da8654c6bb84c6cbd4
Arthur Garin <a.garin@cetera.ru>
2017-07-26 09:40:19 +0300

    https://pm.cetera.ru/browse/WIC-611 Защита персональных данных

--- www/local/templates/.default/components/asd/subscribe.quick.form/.default/template.php
+++ www/local/templates/.default/components/asd/subscribe.quick.form/.default/template.php
@@ -45,7 +45,7 @@ if (method_exists($this, 'setFrameMode')) {
             <input type="checkbox" name="CONFIRM_S" value="Y" data-confirm-input="Y" id="FIELD_CONFIRM_S"
                    class="modal__checkbox">
             <label class="modal__chck " for="FIELD_CONFIRM_S">
-                Я согласен с условиями <a
+                Я принимаю условия <a
                         href="/upload/uf/63d/%D0%9F%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5%20%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5%202017.07.01.pdf"
                         target="_blank">Пользовательского соглашения</a> и <a
                         href="/upload/uf/307/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%202017.07.01.pdf"
