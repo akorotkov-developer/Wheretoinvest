38e406f714dadf2ec9e95f0c9dc603af516b836c
Leonid Chesnokov <leon.chesnokov@gmail.com>
2017-08-10 14:39:50 +0300

    https://pm.cetera.ru/browse/WIC-623

--- www/local/templates/.default/components/asd/subscribe.quick.form/.default/template.php
+++ www/local/templates/.default/components/asd/subscribe.quick.form/.default/template.php
@@ -46,9 +46,9 @@ if (method_exists($this, 'setFrameMode')) {
                    class="modal__checkbox">
             <label class="modal__chck " for="FIELD_CONFIRM_S">
                 Я принимаю условия <a
-                        href="/upload/uf/63d/%D0%9F%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5%20%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5%202017.07.01.pdf"
+                        href="/upload/uf/988/Пользовательское%20соглашение.pdf"
                         target="_blank">Пользовательского соглашения</a> и <a
-                        href="/upload/uf/307/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%202017.07.01.pdf"
+                        href="/upload/uf/4b8/Политика%20конфиденциальности.pdf"
                         target="_blank">Политики конфиденциальности</a>
             </label>
         </div>
--- www/local/templates/.default/components/bitrix/system.auth.registration/.default/template.php
+++ www/local/templates/.default/components/bitrix/system.auth.registration/.default/template.php
@@ -108,7 +108,7 @@ $emailConfirm = COption::GetOptionString("main", "new_user_registration_email_co
                         "REQUIRED" => "Y",
                         "SINGLE" => "Y",
                         "LIST" => Array(
-                            "Y" => 'Я принимаю условия <a href="/upload/uf/63d/%D0%9F%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5%20%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5%202017.07.01.pdf" target="_blank">Пользовательского соглашения</a> и <a href="/upload/uf/307/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%202017.07.01.pdf" target="_blank">Политики конфиденциальности</a>'
+                            "Y" => 'Я принимаю условия <a href="/upload/uf/988/Пользовательское%20соглашение.pdf" target="_blank">Пользовательского соглашения</a> и <a href="/upload/uf/4b8/Политика%20конфиденциальности.pdf" target="_blank">Политики конфиденциальности</a>'
                         ),
                         "PARAMS" => Array(
                             "data-confirm-input" => "Y"
@@ -186,7 +186,7 @@ $emailConfirm = COption::GetOptionString("main", "new_user_registration_email_co
                         "REQUIRED" => "Y",
                         "SINGLE" => "Y",
                         "LIST" => Array(
-                            "Y" => 'Я принимаю условия <a href="/upload/uf/63d/%D0%9F%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5%20%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5%202017.07.01.pdf" target="_blank">Пользовательского соглашения</a> и <a href="/upload/uf/307/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%202017.07.01.pdf" target="_blank">Политики конфиденциальности</a>'
+                            "Y" => 'Я принимаю условия <a href="/upload/uf/988/Пользовательское%20соглашение.pdf" target="_blank">Пользовательского соглашения</a> и <a href="/upload/uf/4b8/Политика%20конфиденциальности.pdf" target="_blank">Политики конфиденциальности</a>'
                         ),
                         "PARAMS" => Array(
                             "data-confirm-input" => "Y"
