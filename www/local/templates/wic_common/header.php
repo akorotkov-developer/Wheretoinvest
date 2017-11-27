974d7442916c3b97cc7ef42cea392b77d59b878b
Leonid Chesnokov <leon.chesnokov@gmail.com>
2017-11-27 10:10:48 +0300

    https://pm.cetera.ru/browse/WIC-642

--- www/local/templates/wic_common/header.php
+++ www/local/templates/wic_common/header.php
@@ -96,10 +96,10 @@ ob_end_clean();
     <title><?php $APPLICATION->ShowTitle() ?></title>
     <link rel="icon" href="/uploads/icons/c76.png" type="image/x-icon">
     <link rel="shortcut icon" href="/uploads/icons/c76.png" type="image/x-icon">
-    <link rel="apple-touch-icon" sizes="72x72" href="/uploads/icons/c72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/uploads/icons/c76.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/uploads/icons/c120m.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/uploads/icons/c152m.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/uploads/icons/i72n.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/uploads/icons/i76n.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/uploads/icons/i120n.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/uploads/icons/i152n.png">
     <? /*?>
     <link rel="apple-touch-icon" href="/uploads/ios-icon-29x29.png">
     <link rel="apple-touch-icon" href="/uploads/ios-icon-40x40.png">
