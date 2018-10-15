f47a690936978f07919380ec9d003342ce36f084
alexander korotkov <89062567799@mail.ru>
2018-10-15 10:45:05 +0300

    Класс Banks 15.10.2018

--- www/local/include/app/BanksInfo/Banks.php
+++ www/local/include/app/BanksInfo/Banks.php
@@ -10,29 +10,184 @@ class Banks
     private $client;
     private $websites;
     private $banks;
+    private $regions;
 
-    function __construct()
-    {
+    function __construct() {
         //Создаем подключение к WSDL серверу
-        $this->client = new \SoapClient("http://cbr.ru/CreditInfoWebServ/CreditOrgInfo.asmx?WSDL");
+        $this->client = new \SoapClient("http://cbr.ru/CreditInfoWebServ/CreditOrgInfo.asmx?WSDL", array('exceptions' => false));
         //Подключение к списку сайтов банков
         $this->websites = new \SimpleXMLElement(file_get_contents("http://www.cbr.ru/credit/GetAsXML.asp"));
         //Подключение к списку банков
         $this->banks = new \SimpleXMLElement(file_get_contents("http://cbr.ru/scripts/XML_bic2.asp"));
     }
 
+    public function test() {
+        //$i = $this->client->GetOfficesByRegionXML(array("RegCode"=>"16"));
+
+        $bankList = $this->banks;
+        return $bankList;
+
+        //return $i;
+
+/*        $intCode = $this->client->BicToIntCode(array("BicCode" => "044525593"))->BicToIntCodeResult;
+
+        $filialInfo = $this->client->GetOfficesXML(array("IntCode" => $intCode))->GetOfficesXMLResult->any;
+        $xml =  new \SimpleXMLElement($filialInfo);
+        $xml->saveXML("regions.xml");
+
+        return $xml;*/
+    }
+
+    private function getFilialInfoByBic($BIC) {
+        $intCode = $this->client->BicToIntCode(array("BicCode" => (string)$BIC))->BicToIntCodeResult;
+
+        $filialInfo = $this->client->GetOfficesXML(array("IntCode" => $intCode));
+
+        return $filialInfo;
+    }
+
     //Перевод строки в транслит
-    private function translit($s) {
-        $s = (string) $s; // преобразуем в строковое значение
-        $s = strip_tags($s); // убираем HTML-теги
-        $s = str_replace(array("\n", "\r"), " ", $s); // убираем перевод каретки
-        $s = preg_replace("/\s+/", ' ', $s); // удаляем повторяющие пробелы
-        $s = trim($s); // убираем пробелы в начале и конце строки
-        $s = function_exists('mb_strtolower') ? mb_strtolower($s) : strtolower($s); // переводим строку в нижний регистр (иногда надо задать локаль)
-        $s = strtr($s, array('а'=>'a','б'=>'b','в'=>'v','г'=>'g','д'=>'d','е'=>'e','ё'=>'e','ж'=>'j','з'=>'z','и'=>'i','й'=>'y','к'=>'k','л'=>'l','м'=>'m','н'=>'n','о'=>'o','п'=>'p','р'=>'r','с'=>'s','т'=>'t','у'=>'u','ф'=>'f','х'=>'h','ц'=>'c','ч'=>'ch','ш'=>'sh','щ'=>'shch','ы'=>'y','э'=>'e','ю'=>'yu','я'=>'ya','ъ'=>'','ь'=>''));
-        $s = preg_replace("/[^0-9a-z-_ ]/i", "", $s); // очищаем строку от недопустимых символов
-        $s = str_replace(" ", "-", $s); // заменяем пробелы знаком минус
-        return $s; // возвращаем результат
+    private function translit($s, $param) {
+        if ($param == "Y") {
+            $s = (string)$s; // преобразуем в строковое значение
+            $s = strip_tags($s); // убираем HTML-теги
+            $s = str_replace(array("\n", "\r"), " ", $s); // убираем перевод каретки
+            $s = preg_replace("/\s+/", ' ', $s); // удаляем повторяющие пробелы
+            $s = trim($s); // убираем пробелы в начале и конце строки
+            $s = function_exists('mb_strtolower') ? mb_strtolower($s) : strtolower($s); // переводим строку в нижний регистр (иногда надо задать локаль)
+            $s = strtr($s, array('а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'e', 'ж' => 'j', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shch', 'ы' => 'y', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya', 'ъ' => '', 'ь' => ''));
+            $s = preg_replace("/[^0-9a-z-_ ]/i", "", $s); // очищаем строку от недопустимых символов
+            $s = str_replace(" ", "-", $s); // заменяем пробелы знаком минус
+            return $s; // возвращаем результат
+        } else {
+            $translit = array(
+                'а' => 'a',   'б' => 'b',   'в' => 'v',
+
+                'г' => 'g',   'д' => 'd',   'е' => 'e',
+
+                'ё' => 'yo',   'ж' => 'zh',  'з' => 'z',
+
+                'и' => 'i',   'й' => 'j',   'к' => 'k',
+
+                'л' => 'l',   'м' => 'm',   'н' => 'n',
+
+                'о' => 'o',   'п' => 'p',   'р' => 'r',
+
+                'с' => 's',   'т' => 't',   'у' => 'u',
+
+                'ф' => 'f',   'х' => 'x',   'ц' => 'c',
+
+                'ч' => 'ch',  'ш' => 'sh',  'щ' => 'shh',
+
+                'ь' => '\'',  'ы' => 'y',   'ъ' => '\'\'',
+
+                'э' => 'e\'',   'ю' => 'yu',  'я' => 'ya',
+
+
+                'А' => 'A',   'Б' => 'B',   'В' => 'V',
+
+                'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
+
+                'Ё' => 'YO',   'Ж' => 'Zh',  'З' => 'Z',
+
+                'И' => 'I',   'Й' => 'J',   'К' => 'K',
+
+                'Л' => 'L',   'М' => 'M',   'Н' => 'N',
+
+                'О' => 'O',   'П' => 'P',   'Р' => 'R',
+
+                'С' => 'S',   'Т' => 'T',   'У' => 'U',
+
+                'Ф' => 'F',   'Х' => 'X',   'Ц' => 'C',
+
+                'Ч' => 'CH',  'Ш' => 'SH',  'Щ' => 'SHH',
+
+                'Ь' => '\'',  'Ы' => 'Y\'',   'Ъ' => '\'\'',
+
+                'Э' => 'E\'',   'Ю' => 'YU',  'Я' => 'YA',
+            );
+
+            $string = strtr($s, $translit);
+            return $string;
+        }
+    }
+
+    //Составление короткого названия из полного
+    private function getShortNameByFullName($FullName) {
+        $name = $FullName;
+        $ShortName = "";
+        $needle = '"';
+        $lastPos = 0;
+        $positions = array();
+
+        while (($lastPos = strpos($name, $needle, $lastPos))!== false) {
+            $positions[] = $lastPos;
+            $lastPos = $lastPos + strlen($needle);
+        }
+
+        $cuurentN = substr($name, $positions[0], $positions[count($positions)-1]-$positions[0]+1);
+
+        if ($positions[0] == 0) {
+            $firstN = false;
+        } else {
+            $firstN = substr($name, 0, $positions[0] - 1);
+        }
+        $lastN = substr($name, $positions[count($positions)-1]+2, strlen($name)-1);
+
+
+        if ($firstN) {
+            $firstN = explode(' ', $firstN);
+            if (count($firstN) > 1) {
+                foreach ($firstN as $i => $element) {
+                    if (strlen($firstN[$i]) > 1) {
+                        $fSymb = substr($firstN[$i], 0, 1);
+                        if ($fSymb != '"') {
+                            if ($fSymb == '(') {
+                                $fSymb = substr($firstN[$i], 1, 1);
+                                $firstN[$i] = '(' . strtoupper($fSymb);
+                            } elseif (substr($firstN[$i], strlen($firstN[$i]) - 1, 1) == ')') {
+                                $fSymb = substr($firstN[$i], 0, 1);
+                                $firstN[$i] = strtoupper($fSymb) . ')';
+                            } else {
+                                $fSymb = substr($firstN[$i], 0, 1);
+                                $firstN[$i] = strtoupper($fSymb);
+                            }
+                        }
+                        $ShortName .= $firstN[$i];
+                    }
+                }
+                $ShortName .= ' ';
+            } else {
+                $ShortName .= $firstN[0].' ';
+            }
+        }
+
+        $ShortName .= $cuurentN;
+
+        if ($lastN != '') {
+            $ShortName .= ' ';
+            $lastN = explode(' ', $lastN);
+            foreach ($lastN as $i => $element) {
+                if (strlen($lastN[$i]) > 1) {
+                    $fSymb = substr($lastN[$i], 0, 1);
+                    if ($fSymb != '"') {
+                        if ($fSymb == '(') {
+                            $fSymb = substr($lastN[$i], 1, 1);
+                            $lastN[$i] = '(' . strtoupper($fSymb);
+                        } elseif (substr($lastN[$i], strlen($lastN[$i]) - 1, 1) == ')') {
+                            $fSymb = substr($lastN[$i], 0, 1);
+                            $lastN[$i] = strtoupper($fSymb) . ')';
+                        } else {
+                            $fSymb = substr($lastN[$i], 0, 1);
+                            $lastN[$i] = strtoupper($fSymb);
+                        }
+                    }
+                    $ShortName .= $lastN[$i];
+                }
+            }
+        }
+
+        return $ShortName;
     }
 
     //Получить сайт организации по BIC
@@ -58,14 +213,23 @@ class Banks
         $response = $response[count($response)-1];
 
         //Данные по форме 135
-        $data = $this->client->Data135FormFullXML(array("CredorgNumber" => $regNumber, "OnDate" => $response));
-        $data = $data->Data135FormFullXMLResult->any;
+        try {
+            $data = $this->client->Data135FormFullXML(array("CredorgNumber" => $regNumber, "OnDate" => $response));
+        } catch (SoapFault $e) {
+            return;
+        }
+        if ($data) {
+            if (get_class($data) != "SoapFault") {
 
-        $xml = new \SimpleXMLElement($data);
+                $data = $data->Data135FormFullXMLResult->any;
 
-        $elements= $xml->xpath("//F135_3[contains(C3,'Н1.0')]");
-        foreach($elements as $element) {
-            $h10 = $element->V3;
+                $xml = new \SimpleXMLElement($data);
+
+                $elements = $xml->xpath("//F135_3[contains(C3,'Н1.0')]");
+                foreach ($elements as $element) {
+                    $h10 = $element->V3;
+                }
+            } else {$h10 = "";}
         }
 
         return $h10;
@@ -80,17 +244,43 @@ class Banks
 
         //Данные по форме 123
         $data = $this->client->Data123FormFullXML(array("CredorgNumber" => $regNumber, "OnDate" => $response));
-        $data = $data->Data123FormFullXMLResult->any;
+        if ($data) {
+            if (get_class($data) != "SoapFault") {
+                $data = $data->Data123FormFullXMLResult->any;
 
-        $xml = new \SimpleXMLElement($data);
-        $xml->saveXML("123.xml");
+                $xml = new \SimpleXMLElement($data);
 
-        $elements = $xml->xpath("//F123[contains(CODE,'000')]");
-        foreach($elements as $element) {
-            $sobcapital = $element->VALUE;
+                $elements = $xml->xpath("//F123[contains(CODE,'000')]");
+                foreach($elements as $element) {
+                    $sobcapital = $element->VALUE;
+                }
+
+                $result["sobcapital"] = $sobcapital;
+                $result["date"] = strtotime($response); // переводит из строки в дату
+                $result["date"] = date("d.m.Y", $result["date"]); // переводит в новый формат
+            } else {$result = "";}
         }
 
-        return $sobcapital;
+
+        return $result;
+    }
+
+    //Дополнительная информация по организации организации по Bic коду
+    public function getOrgInfoByIntCode($BIC) {
+       $info = array();
+
+       $intCode = $this->client->BicToIntCode(array('BicCode' => $BIC));
+
+       $obj = $this->client->CreditInfoByIntCodeXML(array('InternalCode' => $intCode->BicToIntCodeResult));
+       $xml =  new \SimpleXMLElement($obj->CreditInfoByIntCodeXMLResult->any);
+
+       $FullName = $xml->CO->OrgFullName;
+       $ShortName = $this->getShortNameByFullName($FullName);
+
+       $info["ShortName"] = $ShortName;
+       $info["OrgStatus"] = $xml->CO->OrgStatus;
+
+       return $info;
     }
 
     //Обновить пользователей
@@ -100,94 +290,114 @@ class Banks
 
         $i=0;
         foreach ($bankList->Record as $Record) {
+            if ($i > -1) {
+                //Смотрим есть ли уже пользователь для банка
+                $login = $this->translit($Record->ShortName, "Y") . "@wheretoinvest.ru";
+                $password = $this->translit($Record->ShortName);
+                $rsUser = \CUser::GetByLogin($login);
+                $arUser = $rsUser->Fetch();
 
-            //Смотрим есть ли уже пользователь для банка
-            $login = $this->translit($Record->ShortName)."@wheretoinvest.ru";
-            $rsUser = \CUser::GetByLogin($login);
-            $arUser = $rsUser->Fetch();
+                //Получаем все данные для банка
 
-            //Получаем все данные для банка
+                //Информация по филиально сети
+                $filialInfo = $this->getFilialInfoByBic($Record->Bic);
 
-            //Получаем веб-сайт банка
-            $website = $this->getWebSiteOrganization($Record->Bic);
+                //Получаем веб-сайт банка
+                $website = $this->getWebSiteOrganization($Record->Bic);
 
-            //Рег. Номер
-            $regNumber = $this->client->BicToRegNumber(array('BicCode' => (string)$Record->Bic));
-            $regNumber = $regNumber->BicToRegNumberResult;
+                //Получаем дополнительную информацию об организации
+                $info = $this->getOrgInfoByIntCode($Record->Bic);
 
-            //Н1.0 по форме 135
-            $h10 = $this->get135formData($regNumber);
+                //Рег. Номер
+                $regNumber = $this->client->BicToRegNumber(array('BicCode' => (string)$Record->Bic));
+                $regNumber = $regNumber->BicToRegNumberResult;
 
-            //Собственный капитал по форме 123
-            $sobcapital = $this->get123formData($regNumber);
+                //Н1.0 по форме 135
+                echo "H!";
+                echo "<pre>";
+                var_dump($regNumber);
+                echo "</pre>";
+                $h10 = $this->get135formData($regNumber);
 
-            //Активы
-            if ($sobcapital > 0 and $h10 != 0) {
-                $active = $sobcapital / $h10;
-            } else {
-                $active = false;
-            }
+                //Собственный капитал по форме 123
+                $sobcapital = $this->get123formData($regNumber);
 
-            //Если пользователь есть, то обновляем его данные, если нет, то создаем нового пользователя
-            $user = new \CUser;
-
-            if ($arUser) {
-                //ADD USER
-                if (!$active) {
-                    $active = $arUser["UF_ASSETS"];
-                }
-
-                $arFields = Array(
-                    "ACTIVE"            => "Y",
-                    "GROUP_ID"          => array(3,4,6),
-                    "WORK_COMPANY"  => $Record->ShortName,
-                    "UF_SHORT_WORK_EN"  => $this->translit($Record->ShortName),
-                    "UF_FULL_WORK_NAME"  => $Record->ShortName,
-                    "UF_BIK"  => $Record->Bic,
-                    "UF_SITE"  => $website,
-                    "UF_LICENSE"  => $regNumber,
-                    "UF_OGRN"  => $Record->RegNum,
-                    "UF_CAPITAL_ASSETS" => $h10,
-                    "UF_CAPITAL" => $sobcapital,
-                    "UF_ASSETS" => $active,
-                );
-
-                if ($user->Update($arUser["ID"], $arFields)) {
-                    echo "Пользователь: " . $login . " Обновлен";
-                    echo "<br>";
-                }
-            } else {
-                $arFields = Array(
-                    "NAME"              => "Богдан",
-                    "EMAIL"             => $login,
-                    "LOGIN"             => $login,
-                    "ACTIVE"            => "Y",
-                    "GROUP_ID"          => array(3,4,6),
-                    "PASSWORD"          => "123456",
-                    "CONFIRM_PASSWORD"  => "123456",
-                    "WORK_COMPANY"  => $Record->ShortName,
-                    "UF_SHORT_WORK_EN"  => $this->translit($Record->ShortName),
-                    "UF_FULL_WORK_NAME"  => $Record->ShortName,
-                    "UF_BIK"  => $Record->Bic,
-                    "UF_SITE"  => $website,
-                    "UF_LICENSE"  => $regNumber,
-                    "UF_OGRN"  => $Record->RegNum,
-                    "UF_CAPITAL_ASSETS" => $h10,
-                    "UF_CAPITAL" => $sobcapital,
-                    "UF_ASSETS" => $active,
-                );
-
-                $ID = $user->Add($arFields);
-                if (intval($ID) > 0) {
-                    echo "Пользователь: " . $login . " Успешно добавлен.";
-                    echo "<br>";
+                //Активы
+                if ($sobcapital["sobcapital"] > 0 and $h10 != 0) {
+                    $active = ceil($sobcapital["sobcapital"] / ((float)$h10 / 100));
                 } else {
-                    echo $user->LAST_ERROR;
+                    $active = false;
                 }
-            }
 
+                //Если пользователь есть, то обновляем его данные, если нет, то создаем нового пользователя
+                $user = new \CUser;
+
+                if ($arUser) {
+                    //ADD USER
+                    if (!$active) {
+                        $active = $arUser["UF_ASSETS"];
+                    }
+
+                    $arFields = Array(
+                        "ACTIVE" => "Y",
+                        "GROUP_ID" => array(3, 4, 6),
+                        "WORK_COMPANY" => $info["ShortName"],
+                        "UF_SHORT_WORK_EN" => $this->translit($info["ShortName"], "N"),
+                        "UF_FULL_WORK_NAME" => $Record->ShortName,
+                        "UF_BIK" => $Record->Bic,
+                        "UF_SITE" => $website,
+                        "UF_LICENSE" => $regNumber,
+                        "UF_OGRN" => $Record->RegNum,
+                        "UF_CAPITAL_ASSETS" => $h10,
+                        "UF_CAPITAL" => $sobcapital["sobcapital"],
+                        "UF_ASSETS" => $active,
+                        "UF_NOTE" => $info["OrgStatus"],
+                        "UF_ASSETS_DATE" => $sobcapital["date"],
+                        "UF_SIT_CB" => 1,
+                    );
+
+                    if ($user->Update($arUser["ID"], $arFields)) {
+                        echo "Пользователь: " . $login . " Обновлен";
+                        echo "<br>";
+                    }
+                } else {
+                    $arFields = Array(
+                        "NAME" => "Богдан",
+                        "EMAIL" => $login,
+                        "LOGIN" => $login,
+                        "ACTIVE" => "Y",
+                        "GROUP_ID" => array(3, 4, 6),
+                        "PASSWORD" => $password,
+                        "CONFIRM_PASSWORD" => $password,
+                        "WORK_COMPANY" => $info["ShortName"],
+                        "UF_SHORT_WORK_EN" => $this->translit($info["ShortName"]),
+                        "UF_FULL_WORK_NAME" => $Record->ShortName,
+                        "UF_BIK" => $Record->Bic,
+                        "UF_SITE" => $website,
+                        "UF_LICENSE" => $regNumber,
+                        "UF_OGRN" => $Record->RegNum,
+                        "UF_CAPITAL_ASSETS" => $h10,
+                        "UF_CAPITAL" => $sobcapital["sobcapital"],
+                        "UF_ASSETS" => $active,
+                        "UF_NOTE" => $info["OrgStatus"],
+                        "UF_ASSETS_DATE" => $sobcapital["date"],
+                        "UF_SIT_CB" => 1,
+                    );
+
+                    $ID = $user->Add($arFields);
+                    if (intval($ID) > 0) {
+                        echo "Пользователь: " . $login . " Успешно добавлен.";
+                        echo "<br>";
+                    } else {
+                        echo $user->LAST_ERROR;
+                    }
+                }
+
+
+            }
             $i++;
-            if ($i > 5) {
+            echo "<br><b>" . $i . "</b><br>";
+            if ($i > 10) {
                 break;
             }
         }
