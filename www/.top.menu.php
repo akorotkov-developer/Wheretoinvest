da49eca9cc6e79c8321e5368247046f4d76734df
a.bestcennyi <andxbes@ya.ru>
2018-11-12 09:39:54 +0200

    Текудее меню

--- www/.top.menu.php
+++ www/.top.menu.php
@@ -1,39 +1,32 @@
 <?
 $aMenuLinks = Array(
-    Array(
-        "Все способы",
-        "/",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "Банковские<br> вклады",
-        "/?method=3",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "Векселя сертификаты",
-        "/?method=31,32",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "Займы",
-        "/?method=6",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "Избранные",
-        "/?favorite=Y",
-        Array(),
-        Array("class" => "b-header__menu-link_star"),
-        ""
-    )
+	Array(
+		"Общий список", 
+		"/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Вклады", 
+		"/?method=3", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Займы мфо", 
+		"/?method=6", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Избранные", 
+		"/?favorite=Y", 
+		Array(), 
+		Array("class"=>"b-header__menu-link_star"), 
+		"" 
+	)
 );
 ?>
