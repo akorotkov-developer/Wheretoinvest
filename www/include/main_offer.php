52e491e33b78e1284fe57745012fb3d5fe29295a
alexander korotkov <89062567799@mail.ru>
2019-01-12 15:45:54 +0300

    тест

---
+++ www/include/main_offer.php
@@ -0,0 +1,13 @@
+<? require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php"); ?>
+<? $APPLICATION->IncludeComponent(
+    "cetera:super.component",
+    "offer.list",
+    array(
+        "COMPONENT_TEMPLATE" => "offer.list",
+        "CACHE_TYPE" => "N",
+        "CACHE_TIME" => "1",
+        "PAGE_COUNT" => 50,
+        "PAGING" => "Y"
+    ),
+    false
+); ?>
---
+++ www/include/social.php
@@ -0,0 +1,23 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<div>
+	<div class="b-footer__smalltext">
+		Поделиться
+	</div>
+	<script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+	<script src="//yastatic.net/share2/share.js"></script>
+	<div class="ya-share2" data-services="vkontakte,facebook,twitter" data-size="m" data-title="Куда вложить деньги" data-description="Удобный поиск и сравнение инвестиций" data-image="http://wheretoinvest.ru<?=WIC_TEMPLATE_PATH?>/images/wic_logo.png" data-url="http://wheretoinvest.ru/" data-title:twitter="Удобный поиск и сравнение инвестиций" data-image:facebook="http://wheretoinvest.ru<?=WIC_TEMPLATE_PATH?>/images/wic_logo_fb.png"></div>
+</div>
+
+<?/*
+<div class="b-footer__icons">
+    <a href="#" class="b-footer__icon">
+        <img src="<?= WIC_TEMPLATE_PATH ?>/images/icon.png">
+    </a>
+    <a href="#" class="b-footer__icon">
+        <img src="<?= WIC_TEMPLATE_PATH ?>/images/tw.png">
+    </a>
+    <a href="#" class="b-footer__icon">
+        <img src="<?= WIC_TEMPLATE_PATH ?>/images/vk.png">
+    </a>
+</div>
+*/?>
