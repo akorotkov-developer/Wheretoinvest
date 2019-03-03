adcad7d8a21ca6f8ea97eddf559b7128a955a6f1
alexander korotkov <89062567799@mail.ru>
2019-03-03 20:28:10 +0300

    Тест

--- www/local/include/app/BanksInfo/UpdateBanks.php
+++ www/local/include/app/BanksInfo/UpdateBanks.php
@@ -68,7 +68,7 @@ class UpdateBanks implements Interfaces\IUpdateBanks {
                 //Смотрим есть ли уже пользователь для банка
                 $arSpecUser = false;
                 $filter = Array(
-                    "UF_OGRN" => $Record->RegNum,
+                    "UF_LICENSE" => $regNumber,
                 );
                 $rsUsers = \CUser::GetList(($by), ($order = "desc"), $filter);
                 while ($arUser = $rsUsers->Fetch()) {
